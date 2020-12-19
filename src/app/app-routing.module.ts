c8bd3f5336481b96bcd744d23fa1a7e652337001
hautc11 <hautachi@gmai.com>
2020-12-19 21:18:11 +0700

    create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { ContactComponent } from './contact/contact.component';
+import { EventComponent } from './event/event.component';
+import { BarComponent } from './bar/bar.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'hotel', component: HotelComponent},
+  {path:'restaurant', component: RestaurantComponent},
+  {path:'bar', component: BarComponent},
+  {path:'event', component: EventComponent},
+  {path:'contact', component: ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { BarComponent } from './bar/bar.component';
+import { EventComponent } from './event/event.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HotelComponent,
+    RestaurantComponent,
+    BarComponent,
+    EventComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
